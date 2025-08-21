fc2440b4c1966f1ee12d1fb12fe0ae7e844eeaa0
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2025-08-21 23:40:15 +0000

    Aula 21/08/2025

---
+++ public/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Página Principal</title>
+    </head>
+    <body>
+        <h1>Página Principal</h1>
+        <p>Primeiro Exemplo de Pagina Dinâmica</p>
+        <?php
+        //require('./includes/naotem.php');
+        include('./includes/teste.php');
+        include_once('./includes/teste.php');
+        ?>
+    </body>
+</html>
