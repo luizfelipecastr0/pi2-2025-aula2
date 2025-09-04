921ba26ec75d8bca355bcf8728b1a4f7a8dedc8a
LuizFelipeCastro <270644@unifio.edu.br>
2025-09-04 19:47:58 -0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+//require '../vendor/autoload.php';
+//use Application\Hello;
+
+// $_GET é uma variavel de ambiente, existe dentro do php / ?nome=Rafael98
+$nome = $_GET['nome'];
+// '' para strings literais e "" para variaveis.
+$mensagem = "Boa Noite $nome";
+
+$num1 = 10;
+$num2 = 30;
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="/css/default.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP & Mysql</h1>
+    <!-- $mesagem faz escrever oque eu escrevi la em cima na variavel, tem que estar entre os simbulos do php -->
+    <p><?= $mensagem ?></p>
+    <h3>Primeiro teste usando php</h3>
+    <?= $num1 + $num2 ?>
+    <?= $num1 . $num2 ?> <!-- Concatenação com "." -->
+</body>
+</html>
